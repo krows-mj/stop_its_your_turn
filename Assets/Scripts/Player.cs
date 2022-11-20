04a427c4e8647d6264b4ac3b77287b9f4c1ac06c
Krows-MJ <lukakrows@gmail.com>
2022-11-19 21:57:04 -0400

    Sistema de dialogos v1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public Image img;
+
     [SerializeField] private float speedX, speedY;
     [SerializeField] private bool mouse;
     private float horizontal, vertical;
     // Start is called before the first frame update
     void Start()
     {
-        mouse= true;
+        mouse= false;
+        IMGCOLOR();
     }
 
     // Update is called once per frame
@@ -18,11 +22,19 @@ public class Player : MonoBehaviour
     {
         if(Input.GetKeyDown("r")){
             mouse= !mouse;
+            IMGCOLOR();
         }
         if(mouse){
             horizontal += speedX * Input.GetAxis("Mouse X");
             vertical -= speedY * Input.GetAxis("Mouse Y");
             transform.eulerAngles = new Vector3(vertical, horizontal, 0.0f);
+        }
+    }
+    public void IMGCOLOR(){
+        if(mouse){
+            img.color= Color.blue;
+        }else{
+            img.color= Color.white;
         }
     }
 }
---
+++ Assets/Scripts/ctrDialogue.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ctrDialogue : MonoBehaviour
+{
+    [SerializeField] private GameObject panelDialogue;
+    [SerializeField] private Text textAutor;
+    [SerializeField] private Text textDialogue;
+    [SerializeField] private float timeText;
+
+    [System.Serializable]
+    public struct CDialogue{
+        public string autor;
+        [TextArea(2,2)]public string dialogue;
+    }
+    [SerializeField] private CDialogue[] Dialogues;
+
+    [SerializeField] private bool dialogueinit;
+    [SerializeField] private int lineDialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartDialogue();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space") && dialogueinit){
+            if(textDialogue.text == Dialogues[lineDialogue].dialogue){
+                NextDialogue();
+            }else{
+                StopAllCoroutines();
+                textDialogue.text= Dialogues[lineDialogue].dialogue;
+            }
+        }
+    }
+
+    public void StartDialogue(){
+        onPanel();
+        dialogueinit= true;
+        lineDialogue= 0;
+        StartCoroutine(ShowLine());
+    }
+    private IEnumerator ShowLine(){
+        textDialogue.text= string.Empty;
+        textAutor.text= Dialogues[lineDialogue].autor;
+        foreach(char c in Dialogues[lineDialogue].dialogue){
+            textDialogue.text += c;
+            yield return new WaitForSeconds(timeText);
+        }
+    }
+    private void NextDialogue(){
+        lineDialogue++;
+        if(lineDialogue < Dialogues.Length){
+            StartCoroutine(ShowLine());
+        }else{
+            offPanel();
+        }
+    }
+
+    private void onPanel(){
+        panelDialogue.SetActive(true);
+    }
+    private void offPanel(){
+        dialogueinit= false;
+        textAutor.text= string.Empty;
+        textDialogue.text= string.Empty;
+        panelDialogue.SetActive(false);
+    }
+}
